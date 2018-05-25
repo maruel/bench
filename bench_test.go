1cfa64b05f54935260ebab0e6b2c4e757279a3ac
Marc-Antoine Ruel <maruel@chromium.org>
2018-05-24 21:54:32 -0400

    Use sub-benchmark, it makes the output and code clearer

--- bench_test.go
+++ bench_test.go
@@ -5,99 +5,95 @@ import (
 	"testing"
 )
 
-// These two benchmarks are to compare the hypothetical performance of an
+// BenchmarkSliceAddressing compares the hypothetical performance of an
 // image.Gray14 using []uint16 instead of the current []byte.
-
-func BenchmarkSliceUint16(b *testing.B) {
-	pix := make([]uint16, b.N)
-	b.ResetTimer()
-	n := uint16(0)
-	for i := 0; i < b.N; i++ {
-		v := pix[i]
-		n += v
-	}
-	if n != 0 {
-		b.Fatal("Oops")
-	}
-}
-
-func BenchmarkSliceUint8Pairs(b *testing.B) {
-	pix := make([]uint8, b.N*2)
-	b.ResetTimer()
-	n := uint16(0)
-	for i := 0; i < b.N; i++ {
-		v := uint16(2*pix[i+0])<<8 | uint16(pix[2*i+1])
-		n += v
-	}
-	if n != 0 {
-		b.Fatal("Oops")
-	}
-}
-
-// These 3 benchmarks test the different costs for function calls.
-
-func BenchmarkAnonymousFunction(b *testing.B) {
-	b.ReportAllocs()
-	t := 0
-	for i := 0; i < b.N; i++ {
-		f := func(j int) (r int) {
-			r = j * 2
-			return
+func BenchmarkSliceAddressing(b *testing.B) {
+	b.Run("Uint16", func(b *testing.B) {
+		pix := make([]uint16, b.N)
+		b.ResetTimer()
+		n := uint16(0)
+		for i := 0; i < b.N; i++ {
+			v := pix[i]
+			n += v
 		}
-		t = f(i)
-	}
-	fmt.Sprintln(t)
-}
-
-func BenchmarkClosure(b *testing.B) {
-	b.ReportAllocs()
-	t := 0
-	for i := 0; i < b.N; i++ {
-		f := func() (r int) {
-			r = i * 2
-			return
+		if n != 0 {
+			b.Fatal("Oops")
 		}
-		t = f()
-	}
-	fmt.Sprintln(t)
+	})
+	b.Run("Uint8Pair", func(b *testing.B) {
+		pix := make([]uint8, b.N*2)
+		b.ResetTimer()
+		n := uint16(0)
+		for i := 0; i < b.N; i++ {
+			v := uint16(2*pix[i+0])<<8 | uint16(pix[2*i+1])
+			n += v
+		}
+		if n != 0 {
+			b.Fatal("Oops")
+		}
+	})
 }
 
-func BenchmarkNormalFunction(b *testing.B) {
-	b.ReportAllocs()
-	t := 0
-	for i := 0; i < b.N; i++ {
-		t = multiple2(i)
-	}
-	fmt.Sprintln(t)
-}
-
-func BenchmarkNormalFunctionNotInline(b *testing.B) {
-	b.ReportAllocs()
-	t := 0
-	for i := 0; i < b.N; i++ {
-		t = multiple2NoInline(i)
-	}
-	fmt.Sprintln(t)
-}
-
-func BenchmarkReturnedFunction(b *testing.B) {
-	b.ReportAllocs()
-	t := 0
-	f := getFunc()
-	for i := 0; i < b.N; i++ {
-		t = f(i)
-	}
-	fmt.Sprintln(t)
-}
-
-func BenchmarkReturnedFunctionRepeated(b *testing.B) {
-	b.ReportAllocs()
-	t := 0
-	for i := 0; i < b.N; i++ {
+// BenchmarkFunction measures the different costs for function calls.
+func BenchmarkFunction(b *testing.B) {
+	b.Run("Normal", func(b *testing.B) {
+		b.ReportAllocs()
+		t := 0
+		for i := 0; i < b.N; i++ {
+			t = multiple2(i)
+		}
+		fmt.Sprintln(t)
+	})
+	b.Run("NormalNotInline", func(b *testing.B) {
+		b.ReportAllocs()
+		t := 0
+		for i := 0; i < b.N; i++ {
+			t = multiple2NoInline(i)
+		}
+		fmt.Sprintln(t)
+	})
+	b.Run("ClosureArgument", func(b *testing.B) {
+		b.ReportAllocs()
+		t := 0
+		for i := 0; i < b.N; i++ {
+			f := func(j int) (r int) {
+				r = j * 2
+				return
+			}
+			t = f(i)
+		}
+		fmt.Sprintln(t)
+	})
+	b.Run("Closure", func(b *testing.B) {
+		b.ReportAllocs()
+		t := 0
+		for i := 0; i < b.N; i++ {
+			f := func() (r int) {
+				r = i * 2
+				return
+			}
+			t = f()
+		}
+		fmt.Sprintln(t)
+	})
+	b.Run("FunctionPointer", func(b *testing.B) {
+		b.ReportAllocs()
+		t := 0
 		f := getFunc()
-		t = f(i)
-	}
-	fmt.Sprintln(t)
+		for i := 0; i < b.N; i++ {
+			t = f(i)
+		}
+		fmt.Sprintln(t)
+	})
+	b.Run("FunctionPointerRepeated", func(b *testing.B) {
+		b.ReportAllocs()
+		t := 0
+		for i := 0; i < b.N; i++ {
+			f := getFunc()
+			t = f(i)
+		}
+		fmt.Sprintln(t)
+	})
 }
 
 func multiple2(i int) int {
@@ -115,57 +111,57 @@ func getFunc() func(int) int {
 	}
 }
 
-// These benchmarks tests the int64 and float64 division performance. This is
-// because ARM instruction set doesn't declare integer division.
-
-func BenchmarkDivisionInt32(b *testing.B) {
-	t := int32(1<<31 - 1)
-	d := int32(b.N)
-	for i := 0; i < b.N; i++ {
-		t /= d
-	}
-	fmt.Sprintln(t)
+// BenchmarkDivision measures the division performance of various native types.
+// This is because ARM instruction set doesn't declare integer division and
+// Xeon has surprising performance characteristics.
+func BenchmarkDivision(b *testing.B) {
+	b.Run("Int32", func(b *testing.B) {
+		t := int32(1<<31 - 1)
+		d := int32(b.N)
+		for i := 0; i < b.N; i++ {
+			t /= d
+		}
+		fmt.Sprintln(t)
+	})
+	b.Run("Int64", func(b *testing.B) {
+		t := int64(1<<63 - 1)
+		d := int64(b.N)
+		for i := 0; i < b.N; i++ {
+			t /= d
+		}
+		fmt.Sprintln(t)
+	})
+	b.Run("Float32", func(b *testing.B) {
+		t := float32(1e38)
+		d := float32(b.N)
+		for i := 0; i < b.N; i++ {
+			t /= d
+		}
+		fmt.Sprintln(t)
+	})
+	b.Run("Float64", func(b *testing.B) {
+		t := float64(1e308)
+		d := float64(b.N)
+		for i := 0; i < b.N; i++ {
+			t /= d
+		}
+		fmt.Sprintln(t)
+	})
 }
 
-func BenchmarkDivisionInt64(b *testing.B) {
-	t := int64(1<<63 - 1)
-	d := int64(b.N)
-	for i := 0; i < b.N; i++ {
-		t /= d
-	}
-	fmt.Sprintln(t)
-}
-
-func BenchmarkDivisionFloat32(b *testing.B) {
-	t := float32(1e38)
-	d := float32(b.N)
-	for i := 0; i < b.N; i++ {
-		t /= d
-	}
-	fmt.Sprintln(t)
-}
-
-func BenchmarkDivisionFloat64(b *testing.B) {
-	t := float64(1e308)
-	d := float64(b.N)
-	for i := 0; i < b.N; i++ {
-		t /= d
-	}
-	fmt.Sprintln(t)
-}
-
-func BenchmarkShiftInt32(b *testing.B) {
-	t := int32(1<<31 - 1)
-	for i := 0; i < b.N; i++ {
-		t >>= 1
-	}
-	fmt.Sprintln(t)
-}
-
-func BenchmarkShiftInt64(b *testing.B) {
-	t := int64(1<<63 - 1)
-	for i := 0; i < b.N; i++ {
-		t >>= 1
-	}
-	fmt.Sprintln(t)
+func BenchmarkShift(b *testing.B) {
+	b.Run("Int32", func(b *testing.B) {
+		t := int32(1<<31 - 1)
+		for i := 0; i < b.N; i++ {
+			t >>= 1
+		}
+		fmt.Sprintln(t)
+	})
+	b.Run("Int64", func(b *testing.B) {
+		t := int64(1<<63 - 1)
+		for i := 0; i < b.N; i++ {
+			t >>= 1
+		}
+		fmt.Sprintln(t)
+	})
 }
